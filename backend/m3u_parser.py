748a5b439ae3e89f731e1d5d1c43f21a6cef3436
Dipayon <toknowdip@gmail.com>
2026-06-15 21:58:16 +0600

    Add BDIX IPTV playlist from abusaeeidx

--- backend/m3u_parser.py
+++ backend/m3u_parser.py
@@ -236,6 +236,7 @@ async def fetch_and_parse_m3u() -> list[dict]:
         "https://iptv-org.github.io/iptv/index.m3u",
         "https://lupael.github.io/IPTV/running.m3u",
         "https://lupael.github.io/IPTV/world.m3u",
+        "https://github.com/abusaeeidx/Mrgify-BDIX-IPTV/raw/main/playlist.m3u",
     ]
 
     try:
